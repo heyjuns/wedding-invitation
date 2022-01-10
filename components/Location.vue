2719094d64a92c7400cb13885b88cd487bd550ac
Juanda <juanda@trus.co.id>
2022-01-11 00:15:23 +0700

    update map

--- components/Location.vue
+++ components/Location.vue
@@ -18,7 +18,6 @@
           grid
           m-auto
           gap-10
-          pb-10
           grid-cols-1
           md:grid-cols-1
           lg:grid-cols-2 lg:flex lg:items-stretch lg:gap-28
@@ -47,7 +46,8 @@
               Akad Nikah
             </h1>
             <h2 class="text-xl md:text-2xl">
-              Masjid Jami Al-Amin, RW 03, Pondok Permai, Kutabaru, Tangerang.
+              Lapangan Serba guna Masjid Jami Al-Amin, RW 03, Pondok Permai,
+              Kutabaru, Tangerang.
             </h2>
             <h3 class="text-xl md:text-2xl pt-4">Sabtu, 5 Maret 2022</h3>
             <h4 class="text-xl md:text-2xl">09.00 - 10.00</h4>
@@ -96,6 +96,30 @@
             <h4 class="text-xl md:text-2xl">11.00 - 16.00</h4>
           </div>
         </section>
+      </div>
+
+      <div
+        class="
+          flex
+          items-center
+          mx-auto
+          my-8
+          justify-center
+          shadow-2xl
+          bg-white
+          w-11/12
+          md:w-8/12
+          lg:w-6/12
+          rounded-xl
+        "
+      >
+        <iframe
+          class="w-full aspect-video p-4 md:p-6"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d991.7063416152381!2d106.5725372117676!3d-6.154138155062919!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ff852e4779bd%3A0x3065bd668cdd8fa3!2sJl.%20Bawal%20IV%20Blok%20B4%20No.20%2C%20Kuta%20Baru%2C%20Kec.%20Ps.%20Kemis%2C%20Kabupaten%20Tangerang%2C%20Banten%2015560!5e0!3m2!1sid!2sid!4v1641831456275!5m2!1sid!2sid"
+          style="border: 0"
+          allowfullscreen=""
+          loading="lazy"
+        ></iframe>
       </div>
     </div>
   </div>
