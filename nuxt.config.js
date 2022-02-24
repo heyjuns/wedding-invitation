acb6bfe50243e995e7052afd7e61fecfcb1cf83c
Juanda <heyjuns@icloud.com>
2022-02-24 14:18:02 +0700

    update desc

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,11 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Prien & Juanda',
+    title: 'Undangan Pernikahan',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Undangan Pernikahan Prien Novita Ayu Cahyaningrum & Juanda Leonardo' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
@@ -72,7 +72,7 @@ export default {
       lang: 'en',
     },
     icon: {
-      purpose:'maskable'
+      purpose: 'maskable'
     }
   },
 
