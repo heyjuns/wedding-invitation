2459741858da2f88cea89279363749c8f0c17c93
Juanda <heyjuns@icloud.com>
2022-02-24 13:41:09 +0700

    update pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,14 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'limamaret',
+    title: 'Prien & Juanda',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -71,6 +71,9 @@ export default {
     manifest: {
       lang: 'en',
     },
+    icon: {
+      purpose:'maskable'
+    }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
